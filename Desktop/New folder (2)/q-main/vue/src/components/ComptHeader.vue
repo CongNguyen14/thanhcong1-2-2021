72b86258dc3c003b12fe46573319affc462d7303
cong <cong@gmail.com>
2020-11-13 15:06:40 +0700

    as

---
+++ Desktop/New folder (2)/q-main/vue/src/components/ComptHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1 >{{msg}}</h1>
+    </header> 
+</template>
+
+<script>
+export default {
+    name: 'comp-header',
+    props:['msg'],
+    data () {
+        return {
+            
+        }
+    }
+
+}
+</script>
+
